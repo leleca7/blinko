ae7933eb79a0356d9ac0887cbd197ca33ab2feb3
leleca7 <wanelle52@gmail.com>
2026-08-27 21:23:27 -0300

    fix: corrigir imports do pagamento de diagnóstico

--- app/api/interno/pre-diagnosticos/[id]/diagnostic/payment/route.ts
+++ app/api/interno/pre-diagnosticos/[id]/diagnostic/payment/route.ts
@@ -3,8 +3,8 @@ import {
   confirmBlinkoDiagnosticPayment,
   getBlinkoDiagnosticContext,
   isDiagnosticSchemaPending,
-} from "../../../../../../../../lib/blinko/diagnostic-commercial";
-import { getInternalSession } from "../../../../../../../../lib/blinko/internal-auth";
+} from "../../../../../../../lib/blinko/diagnostic-commercial";
+import { getInternalSession } from "../../../../../../../lib/blinko/internal-auth";
 
 const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
